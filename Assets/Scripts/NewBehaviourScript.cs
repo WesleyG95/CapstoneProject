f52d541fc816681c0e75dd066228474d5a1898d2
Wesley Gill <WesleyG95@hotmail.com>
2016-03-23 12:45:46 -0400

    Initial Commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    // Normal Movements Variables
+    public float speed = 10;
+    bool facingRight = true;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("SpeedH", Mathf.Abs(moveH));
+        anim.SetFloat("SpeedV", moveV);
+
+        // Move senteces
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveH * speed, moveV * speed);
+
+        if (moveH > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveH < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
