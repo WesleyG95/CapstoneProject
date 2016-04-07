dc7842ad28255af2d6d3cf75e911c81987a32be4
Alanna <akwaltz13@gmail.com>
2016-04-07 14:33:27 -0400

    Title screen added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public bool isStart;
+    public bool isQuit;
+    // Use this for initialization
+	void Start () {
+
+	}
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    } 
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseHover : MonoBehaviour {
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.gray;
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.gray;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
