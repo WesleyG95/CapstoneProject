bbd6df7d8dd5cab0fd293706e2d5b9ff4824fe35
Wesley Gill <WesleyG95@hotmail.com>
2016-04-12 13:24:20 -0400

    Day 12.1

---
+++ Assets/LockedDoorScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LockedDoorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        bool isEnemies = true;
+        isEnemies = checkForEnemies();
+	}
+
+    bool checkForEnemies()
+    {
+        GameObject[] a = new GameObject[]();
+        a = GameObject.FindGameObjectsWithTag("Enemy");
+        return true;
+    }
+}
