d50065dada02f40c40a2bab55b215359dc18c337
Wesley Gill <WesleyG95@hotmail.com>
2016-04-25 13:49:58 -0400

    Day 18.1

---
+++ Assets/Scripts/HealthIndicatorScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthIndicatorScript : MonoBehaviour {
+
+    public Sprite HighHealth;
+    public Sprite MediumHealth;
+    public Sprite LowHealth;
+
+    EnemyAI enemyScript;
+
+    double halfHealth;
+    double quarterHealth;
+
+	// Use this for initialization
+	void Start () {
+        enemyScript = this.GetComponentInParent<EnemyAI>();
+        halfHealth = enemyScript.health / 2;
+        quarterHealth = enemyScript.health / 4;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enemyScript.health <= halfHealth)
+        {
+            this.GetComponentInParent<SpriteRenderer>().sprite = MediumHealth;
+        }
+        else if (enemyScript.health <= quarterHealth)
+        {
+            this.GetComponentInParent<SpriteRenderer>().sprite = LowHealth;
+        }
+        else
+        {
+            this.GetComponentInParent<SpriteRenderer>().sprite = HighHealth;
+        }
+	}
+}
