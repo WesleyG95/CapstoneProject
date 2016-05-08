10190cb9062862353e44970fd92f695c241dd32a
Alanna <akwaltz13@gmail.com>
2016-05-08 15:35:37 -0400

    More storyblocks
    StoryBlock 2 is empty right now and not part of the build
    Final storyblock has replaced "You Win" screen in build

--- Assets/Scripts/TextAnimation.cs
+++ Assets/Scripts/TextAnimation.cs
@@ -8,16 +8,33 @@ public class TextAnimation : MonoBehaviour
     public float letterPause = 0.03f;
     public GUIStyle font;
     public GUIStyle skip;
-    string story1 = "{Player} made it.  The portal worked and he survived.  A few echoing cries from the other side carried through behind him- but for only a few moments before they dissipated into the silence of the small room.  It looked like the castle, but it wasn't.  His memory was suddenly foggy but he held tight a specific voice- the advisor's dying words.";
-    string story2 = "The sword. It was the only way he could have even a chance against the dark army's commander and, if it was even possible, to free himself from the corruption of darkness that was stealing his soul.  If {Player} couldn't stop him, the army of this altered world would overrun and destroy the barrier, uniting their world with {Player}'s.  It would spill perpetual shadow across {World} and give free range to all its dark inhabitants.  It would be the end of everything.  It already practically destroyed the castle.  All he had to do was beat the Dark Commander to the sword.  If {Player} got to it first, he could stop all this....";
+    string story1_1 = "{Player} made it.  The portal worked and he survived.  A few echoing cries from the other side carried through behind him- but for only a few moments before they dissipated into the silence of the small room.  It looked like the castle, but it wasn't.  His memory was suddenly foggy but he held tight a specific voice- the advisor's dying words.";
+    string story1_2 = "The sword. It was the only way he could have even a chance against the dark army's commander and, if it was even possible, to free himself from the corruption of darkness that was stealing his soul.  If {Player} couldn't stop him, the army of this altered world would overrun and destroy the barrier, uniting their world with {Player}'s.  It would spill perpetual shadow across {World} and give free range to all its dark inhabitants.  It would be the end of everything.  It already practically destroyed the castle.  All he had to do was beat the Dark Commander to the sword.  If {Player} got to it first, he could stop all this....";
+
+
+    string storyFinal1 = "The Dark Commander falls!  Upon entering the final door, leaving the commander's broken body behind, {Player} was spit back into his world near to where he first departed.  He stood from his knees, scanning the grand throne room.  The dust of demons departed, shimmering in the sun that was again beaming through the colored windows, was still drifting through the room.  They were gone, and light returned to {World}.  But the ones lost were not to be revived, and their count was sickening.";
+    string storyFinal2 = "{Player} stood in a daze for a while, unsure of how to feel, but living voices pushed him forward.  The court mage... she survived!  The fact that she did was enough of a relief.  At least someone did.  She and the rest of the world would breathe another day to remember this hell, while {Player} would relive it nightmares for some time to come.";
+    string storyFinal3 = "Victory came with a heavy price....  'Were there ever any winners in war?' {Player} wondered.  How could there be?  It would take years before {World} would ever feel normal again.";
+
     string text;
     string skipText = "Skip: Space";
     int spacePressedCount = 0;
 
     void Start()
     {
-        text = "";
-        StartCoroutine(TypeText(story1));
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "StoryBlock1")
+        {
+            text = "";
+            StartCoroutine(TypeText(story1_1));
+        }
+        //more story blocks here
+
+        else if (currentScene.name == "FinalStory")
+        {
+            text = "";
+            StartCoroutine(TypeText(storyFinal1));
+        }
     }
 
     IEnumerator TypeText(string story)
@@ -37,36 +54,94 @@ public class TextAnimation : MonoBehaviour
 
     void Update()
     {
-        //completes the story instead of typing it out
-        if (Input.GetKeyUp(KeyCode.Space)) 
+        //get the current scene
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        if (currentScene.name == "StoryBlock1")
         {
-            if (spacePressedCount == 0)
+            if (Input.GetKeyUp(KeyCode.Space))
             {
-                spacePressedCount++;
-                StopAllCoroutines();
-                text = story1;
-            }
+                //player skipped text typing and sees the entire storyblock1
+                if (spacePressedCount == 0)
+                {
+                    spacePressedCount++;
+                    StopAllCoroutines();
+                    text = story1_1;
+                }
 
-            else if ((spacePressedCount == 1) && (text == story1))
-            {
-                spacePressedCount++;
-                StopAllCoroutines();
-                text = "";
-                StartCoroutine(TypeText(story2));
-            }
+                //start storyblock1 part 2
+                else if ((spacePressedCount == 1) && (text == story1_1))
+                {
+                    spacePressedCount++;
+                    StopAllCoroutines();
+                    text = "";
+                    StartCoroutine(TypeText(story1_2));
+                }
 
-            else if (spacePressedCount == 2)
-            {
-                spacePressedCount++;
-                StopAllCoroutines();
-                text = story2;
-            }
+                else if (spacePressedCount == 2)
+                {
+                    spacePressedCount++;
+                    StopAllCoroutines();
+                    text = story1_2;
+                }
 
-            else if ((spacePressedCount == 3) && (text == story2))
-            {
-                SceneManager.LoadScene("Room1");
+                //story is finished and the game begins
+                else if ((spacePressedCount == 3) && (text == story1_2))
+                {
+                    SceneManager.LoadScene("Room1");
+                }
+                else { Debug.Log("Huge Error- storyblock1"); }
             }
-            else { Debug.Log("Huge Error"); }
+        }
+        else if (currentScene.name == "StoryBlock2")
+        {
+            //stuff to come
+        }
+        else if (currentScene.name == "FinalStory")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                if (spacePressedCount == 0)
+                {
+                    spacePressedCount++;
+                    StopAllCoroutines();
+                    text = storyFinal1;
+                }
+                //start final story part 2
+                else if ((spacePressedCount == 1) && (text == storyFinal1))
+                {
+                    spacePressedCount++;
+                    StopAllCoroutines();
+                    text = "";
+                    StartCoroutine(TypeText(storyFinal2));
+                }
+                else if (spacePressedCount == 2)
+                {
+                    spacePressedCount++;
+                    StopAllCoroutines();
+                    text = storyFinal2;
+                }
+                //start final story part 3
+                else if ((spacePressedCount == 3) && (text == storyFinal2))
+                {
+                    spacePressedCount++;
+                    StopAllCoroutines();
+                    text = "";
+                    StartCoroutine(TypeText(storyFinal3));
+                }
+
+                else if (spacePressedCount == 4)
+                {
+                    spacePressedCount++;
+                    StopAllCoroutines();
+                    text = storyFinal3;
+                }
+                else if ((spacePressedCount == 5) && (text == storyFinal2))
+                {
+                    //load credits
+                    Application.Quit();
+                }
+            }
         }
     }
 }
