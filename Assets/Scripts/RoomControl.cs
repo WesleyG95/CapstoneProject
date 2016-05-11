875d8ee3997d86be178d8ed2bb0f9dce292bcee2
Wesley Gill <WesleyG95@hotmail.com>
2016-05-11 10:58:00 -0400

    Day 24

---
+++ Assets/Scripts/RoomControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+
+    void Awake()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
