4b3554d1d416dff1fb57b23bf2e8c84dc4820aa2
Alanna <akwaltz13@gmail.com>
2016-05-12 01:13:42 -0400

    Story edited
    Nearly finished.  If we can't, don't want to, or don't have the time to
    change the final scene (pick up new sword, spawn boss, etc) then I will
    change it so it works.

--- Assets/Scripts/TextAnimation.cs
+++ Assets/Scripts/TextAnimation.cs
@@ -8,13 +8,12 @@ public class TextAnimation : MonoBehaviour
     public float letterPause = 0.03f;
     public GUIStyle font;
     public GUIStyle skip;
-    string story1_1 = "{Player} made it.  The portal worked and he survived.  A few echoing cries from the other side carried through behind him- but for only a few moments before they dissipated into the silence of the small room.  It looked like the castle, but it wasn't.  His memory was suddenly foggy but he held tight a specific voice- the advisor's dying words.";
-    string story1_2 = "The sword. It was the only way he could have even a chance against the dark army's commander and, if it was even possible, to free himself from the corruption of darkness that was stealing his soul.  If {Player} couldn't stop him, the army of this altered world would overrun and destroy the barrier, uniting their world with {Player}'s.  It would spill perpetual shadow across {World} and give free range to all its dark inhabitants.  It would be the end of everything.  It already practically destroyed the castle.  All he had to do was beat the Dark Commander to the sword.  If {Player} got to it first, he could stop all this....";
+    string story1_1 = "Sunil made it.  The portal worked and he survived.  A few echoing cries from the other side carried through behind him- but for only a few moments before they dissipated into the silence of the small room.  It looked like the castle, but it wasn't.  His memory was suddenly foggy but he held tight a specific voice- the advisor's dying words.";
+    string story1_2 = "The sword. It was the only way he could have even a chance against the dark army's commander and, if it was even possible, to free himself from the corruption of darkness that was stealing his soul.  If Sunil couldn't stop him, the army of this altered world would overrun and destroy the barrier, uniting their world with Sunil's.  It would spill perpetual shadow across {World} and give free range to all its dark inhabitants.  It would be the end of everything.  It already practically destroyed the castle.  All he had to do was beat the Dark Commander to the sword.  If Sunil got to it first, he could stop all this....";
 
 
-    string storyFinal1 = "The Dark Commander falls!  Upon entering the final door, leaving the commander's broken body behind, {Player} was spit back into his world near to where he first departed.  He stood from his knees, scanning the grand throne room.  The dust of demons departed, shimmering in the sun that was again beaming through the colored windows, was still drifting through the room.  They were gone, and light returned to {World}.  But the ones lost were not to be revived, and their count was sickening.";
-    string storyFinal2 = "{Player} stood in a daze for a while, unsure of how to feel, but living voices pushed him forward.  The court mage... she survived!  The fact that she did was enough of a relief.  At least someone did.  She and the rest of the world would breathe another day to remember this hell, while {Player} would relive it nightmares for some time to come.";
-    string storyFinal3 = "Victory came with a heavy price....  'Were there ever any winners in war?' {Player} wondered.  How could there be?  It would take years before {World} would ever feel normal again.";
+    string storyFinal1 = "The Dark Commander falls!  Upon entering the final door, leaving the commander's broken body behind, Sunil was spit back into his world near to where he first departed.  He stood from his knees, scanning the grand throne room.  The dust of demons departed, shimmering in the sun that was again beaming through the colored windows, was still drifting through the room.  They were gone, and light returned to {World}.  But the ones lost were not to be revived, and their count was sickening.";
+    string storyFinal2 = "Sunil stood in a daze for a while, unsure of how to feel, but living voices pushed him forward.  The court mage... she survived!  The fact that she did was enough of a relief.  At least someone did.  She and the rest of the world would breathe another day to remember this hell, while Sunil would relive it nightmares for some time to come.  Victory came with a heavy price... as it always did and will.";
 
     string text;
     string skipText = "Skip: Space";
@@ -31,7 +30,6 @@ public class TextAnimation : MonoBehaviour
             text = "";
             StartCoroutine(TypeText(story1_1));
         }
-        //more story blocks here
 
         else if (currentScene.name == "FinalStory")
         {
@@ -119,20 +117,7 @@ public class TextAnimation : MonoBehaviour
                     spacePressed();
                     text = storyFinal2;
                 }
-                //start final story part 3
                 else if ((spacePressedCount == 3) && (text == storyFinal2))
-                {
-                    spacePressed();
-                    text = "";
-                    StartCoroutine(TypeText(storyFinal3));
-                }
-
-                else if (spacePressedCount == 4)
-                {
-                    spacePressed();
-                    text = storyFinal3;
-                }
-                else if ((spacePressedCount == 5) && (text == storyFinal2))
                 {
                     //load credits
                     Application.Quit();
