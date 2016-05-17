8f45cf00c4e478e129e8eb722627922fa53aa76b
Alanna <akwaltz13@gmail.com>
2016-05-17 11:56:19 -0400

    Credits fixed and ready

--- Assets/Scripts/ScrollingText.cs
+++ Assets/Scripts/ScrollingText.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ScrollingText : MonoBehaviour {
 
     public GameObject camera;
     public int speed = 1;
 	
+    void Start()
+    {
+        StartCoroutine("WaitForFinish");
+    }
+
 	// Update is called once per frame
 	void Update () {
         camera.transform.Translate(Vector2.down * Time.deltaTime * speed);
 
-        //get some time to display it all and when it's done
-        //load the main menu
+        
 	}
+    IEnumerator WaitForFinish()
+    {
+        yield return new WaitForSeconds(30);
+
+        SceneManager.LoadScene("mainmenu2");
+    }
 }
