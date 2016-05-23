9dec62648f18adf9e64f1eba8b60d7265b18200b
Wesley Gill <WesleyG95@hotmail.com>
2016-05-23 13:45:45 -0400

    Day 30.5

--- Assets/Scripts/TextAnimation.cs
+++ Assets/Scripts/TextAnimation.cs
@@ -8,6 +8,9 @@ public class TextAnimation : MonoBehaviour
     public float letterPause = 0.02f;
     public GUIStyle font;
     public GUIStyle skip;
+
+    Scene currentScene;
+
     string story1_1 = "Sunil made it.  The portal worked and he survived.  A few echoing cries from the other side carried through behind him- but for only a few moments before they dissipated into the silence of the small room.  It looked like the castle, but it wasn't.  \n\nHis memory was suddenly foggy; he held tight a specific voice- the advisor's dying words. \n\nThe sword. It was the only way he could have even a chance against the dark army's commander Erebus and, if it was even possible, to free himself from the corruption of darkness that was stealing his soul. \n\nIf Sunil couldn't stop him, the army of this altered world would overrun and destroy the barrier, uniting their world with Sunil's.  It would spill perpetual shadow across Ardengard and give free range to all its dark inhabitants.  It would be the end of everything.";
     string story1_2 = "It already practically destroyed the castle.  The king, his wife and daughter, nearly all of his court and guard- gone.  The court mage, Runa, was the only one Sunil could find among the dead.\n\nWith a powerful force of unknown origin, Commander Erebus managed to open a gate to the world of light and allowed tens of his minions to unexpectedly enter the castle.  By the same force weilded by the castle's court mage, Sunil was allowed to enter this world the same way.  \n\nHe left his world of light and entered the darness- even as soldiers died behind him.  All he had to do was beat the Dark Commander to the sword.  If Sunil got to it first, he could stop all this....";
 
@@ -20,9 +23,10 @@ public class TextAnimation : MonoBehaviour
 
     void Start()
     {
+        currentScene = SceneManager.GetActiveScene();
+
         StopAllCoroutines();
-        //get the current scene
-        Scene currentScene = SceneManager.GetActiveScene();
+
         //check what scene is active
         if (currentScene.name == "StoryBlock1")
         {
@@ -51,13 +55,17 @@ public class TextAnimation : MonoBehaviour
 
     void OnGUI()
     {
-        GUI.Label(new Rect(100, 80, 2000, 3000), text, font);
-        GUI.Label(new Rect(1000, 600, 200, 100), skipText, skip);
+        if (currentScene.name != "Tutorial")
+        {
+            GUI.Label(new Rect(100, 80, 2000, 3000), text, font);
+            GUI.Label(new Rect(1000, 600, 200, 100), skipText, skip);
+        }
     }
 
     void Update()
     {
-        Scene currentScene = SceneManager.GetActiveScene();
+        currentScene = SceneManager.GetActiveScene();
+
         if (currentScene.name == "StoryBlock1")
         {
             //space bar is pressed
@@ -119,6 +127,13 @@ public class TextAnimation : MonoBehaviour
                     SceneManager.LoadScene(RoomControl.creditsScene);
                 }
                 else { Debug.Log("Error- final storyblock"); }
+            }
+        }
+        else if (currentScene.name == "Tutorial")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                RoomControl.loadNewScene();
             }
         }
     }
