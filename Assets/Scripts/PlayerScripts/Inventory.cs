595b5bb2f341693305b66247e9bdc412db4d30f8
Wesley Gill <WesleyG95@hotmail.com>
2016-05-23 12:10:54 -0400

    Day 30.4

--- Assets/Scripts/PlayerScripts/Inventory.cs
+++ Assets/Scripts/PlayerScripts/Inventory.cs
@@ -7,9 +7,6 @@ public class Inventory : MonoBehaviour
     public int HealthInv = 0;
     public int PowerInv = 0;
     public int HealthPotionHeal = 25;
-    public GUIStyle style;
-    public GUIStyle style2;
-    public bool showGUI = false;
     PlayerScript playerScript;
 
     void Start()
@@ -19,30 +16,9 @@ public class Inventory : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (showGUI == false)
-            {
-                showGUI = true;
-            }
-            else {
-                showGUI = false;
-            }
-        }
-
-        if ((showGUI == true) && (Input.GetKeyDown(KeyCode.F)) && (HealthInv > 0) && (playerScript.health < playerScript.maxHealth))
+        if ((Input.GetKeyDown(KeyCode.F)) && (HealthInv > 0) && (playerScript.health < playerScript.maxHealth))
         {
             usePotion();
-        }
-    }
-
-    void OnGUI()
-    {
-        if (showGUI == true)
-        {
-            GUI.Box(new Rect(20, 40, 50, 50), "Inventory", style);
-            GUI.Label(new Rect(20, 90, 60, 60), "Health Potions(F):", style2);
-            GUI.Label(new Rect(280, 90, 40, 30), " " + HealthInv, style2);
         }
     }
 
