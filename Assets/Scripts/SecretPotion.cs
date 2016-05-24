b5e2eab7d15b6b283cf18e0b389e731032ab6d51
mkody91 <kmiller91@southhills.edu>
2016-05-24 14:10:28 -0400

    Start of Hidden Potion

---
+++ Assets/Scripts/SecretPotion.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretPotion : RemovableObjects
+{
+
+    int health = 10;
+ 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (health <= 0)
+        {
+            Die();
+        }
+        Debug.Log(health);
+	
+	}
+}
